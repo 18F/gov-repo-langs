6ef6e2be1bd6d3612ad3596c452e356ce9bb0db0
Greg Walker <michael.walker@gsa.gov>
2016-07-22 09:03:21 -0500

    Get rid of globals; create main() function and execute if this module is execute directly.  Closes #1.

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ import base64
 import yaml
 import time
 from functools import reduce
-
 import json
 
-clientID = os.environ.get('OAUTH_CLIENT_ID');
-clientSecret = os.environ.get('OAUTH_CLIENT_SECRET');
-authFragment = 'client_id={0}&client_secret={1}'.format(clientID, clientSecret);
+def getAuthFragment():
+    clientID = os.environ.get('OAUTH_CLIENT_ID');
+    clientSecret = os.environ.get('OAUTH_CLIENT_SECRET');
+    return 'client_id={0}&client_secret={1}'.format(clientID, clientSecret);
 
 def get_gov_orgs():
     time.sleep(1)
-    return yaml.safe_load(base64.b64decode(requests.get('https://api.github.com/repos/github/government.github.com/contents/_data/governments.yml?{0}'.format(authFragment)).json()['content']))
+    return yaml.safe_load(base64.b64decode(requests.get('https://api.github.com/repos/github/government.github.com/contents/_data/governments.yml?{0}'.format(getAuthFragment())).json()['content']))
 
 def get_repos_in_org(org, page=1):
     if page == 1:
         print('Getting repos for {0}...'.format(org))
     time.sleep(1)
-    response = requests.get('https://api.github.com/orgs/{0}/repos?{2}&page={1}'.format(org, page, authFragment))
+    response = requests.get('https://api.github.com/orgs/{0}/repos?{2}&page={1}'.format(org, page, getAuthFragment()))
 
     if page == 1:
         fullList = response.json()
@@ -41,35 +41,39 @@ def get_repos_in_org(org, page=1):
 def get_langs_in_repo(org, repo):
     print('  -> Getting languages for {0}/{1}'.format(org, repo))
     time.sleep(1)
-    return requests.get('https://api.github.com/repos/{0}/{1}/languages?{2}'.format(org, repo, authFragment)).json()
+    return requests.get('https://api.github.com/repos/{0}/{1}/languages?{2}'.format(org, repo, getAuthFragment())).json()
 
-entities = [
-    { 'name': 'US federal government', 'sections': [ 'U.S. Federal', 'U.S. Military and Intelligence' ] },
-    { 'name': 'US states', 'sections': [ 'U.S. States' ] },
-    { 'name': 'US counties and cities', 'sections': [ 'U.S. County', 'U.S. City' ] },
-    { 'name': 'US tribal nations', 'sections': [ 'U.S. Tribal Nations' ] }
-]
+def main():
+    entities = [
+        { 'name': 'US federal government', 'sections': [ 'U.S. Federal', 'U.S. Military and Intelligence' ] },
+        { 'name': 'US states', 'sections': [ 'U.S. States' ] },
+        { 'name': 'US counties and cities', 'sections': [ 'U.S. County', 'U.S. City' ] },
+        { 'name': 'US tribal nations', 'sections': [ 'U.S. Tribal Nations' ] }
+    ]
 
-gov_orgs = get_gov_orgs()
+    gov_orgs = get_gov_orgs()
 
-for level in entities:
-    level['orgs'] = { }
-    for section in level['sections']:
-        for org in gov_orgs[section]:
-            level['orgs'][org] = { 'repos': [ ] }
+    for level in entities:
+        level['orgs'] = { }
+        for section in level['sections']:
+            for org in gov_orgs[section]:
+                level['orgs'][org] = { 'repos': [ ] }
 
-for org in entities[0]['orgs']:
-    repos = get_repos_in_org(org)
-    print('--- Got {0} repos for {1}'.format(len(repos), org))
-    entities[0]['orgs'][org]['repos'] = [{ 'name': repo['name'], 'desc': repo['description'], 'url': repo['html_url'], 'langs': get_langs_in_repo(org, repo['name']) } for repo in repos]
+    for org in entities[0]['orgs']:
+        repos = get_repos_in_org(org)
+        print('--- Got {0} repos for {1}'.format(len(repos), org))
+        entities[0]['orgs'][org]['repos'] = [{ 'name': repo['name'], 'desc': repo['description'], 'url': repo['html_url'], 'langs': get_langs_in_repo(org, repo['name']) } for repo in repos]
 
-    for repo in entities[0]['orgs'][org]['repos']:
-        if bool(repo['langs']):
-            total = reduce((lambda x, y: x + y), [repo['langs'][lang] for lang in repo['langs']])
-            repo['langs'] = [{ 'name': lang, 'lines': repo['langs'][lang], 'pct': round(repo['langs'][lang] / total, 3) } for lang in repo['langs']]
-        else:
-            repo['langs'] = [ ]
+        for repo in entities[0]['orgs'][org]['repos']:
+            if bool(repo['langs']):
+                total = reduce((lambda x, y: x + y), [repo['langs'][lang] for lang in repo['langs']])
+                repo['langs'] = [{ 'name': lang, 'lines': repo['langs'][lang], 'pct': round(repo['langs'][lang] / total, 3) } for lang in repo['langs']]
+            else:
+                repo['langs'] = [ ]
 
-outFile = open('data/langs.json', 'w');
-outFile.write(json.dumps(entities, sort_keys=True, indent=2))
-outFile.close()
+    outFile = open('data/langs.json', 'w');
+    outFile.write(json.dumps(entities, sort_keys=True, indent=2))
+    outFile.close()
+
+if __name__ == '__main__':
+    main()
